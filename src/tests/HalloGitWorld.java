f2885b43e956ff728f8ecc2dee4e5ef864a08205
sahin1201 <sahinson_1987@hotmail.com>
2020-05-06 16:45:00 +0200

    update login, HalloGitWorld.java

--- src/tests/HalloGitWorld.java
+++ src/tests/HalloGitWorld.java
@@ -5,6 +5,13 @@ public class HalloGitWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello new world againg");
+        System.out.println("Hello new world againg");
+
+
+
+
+
+
 
     }
 }
