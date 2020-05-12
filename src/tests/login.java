b2629dd72331ea0e880a483b9bfa4dd75301559e
Sahin <62034696+sahin1201@users.noreply.github.com>
2020-05-12 17:06:49 +0200

    Update login.java

--- src/tests/login.java
+++ src/tests/login.java
@@ -5,5 +5,8 @@ public class login {
     public static void main(String[] args) {
         System.out.println("Hello login");
         System.out.println("Hello login");
+        
+        //HALLO WORLD
+        System.out.println("Hallo github");
     }
 }
